70b47923f019c1b1cd17a4586944a7794d5982e9
Tung Pham <tung.42@gmail.com>
2017-10-20 10:15:34 +0700

    clock

--- templates/clock.tpl.php
+++ templates/clock.tpl.php
@@ -1,23 +1,15 @@
 <div id="clock">
-	<div class="clock-circle">
-		<div class="clock-face">
-			<ul class="clock-digits">
-				<li>1</li>
-				<li>2</li>
-				<li>3</li>
-				<li>4</li>
-				<li>5</li>
-				<li>6</li>
-				<li>7</li>
-				<li>8</li>
-				<li>9</li>
-				<li>10</li>
-				<li>11</li>
-				<li>12</li>
-			</ul>
-			<div id="hour" class="clock-hour"></div>
-			<div id="minute" class="clock-minute"></div>
-			<div id="second" class="clock-second"></div>
+	<link rel="stylesheet" href="/css/css_clock.css">
+	<article class="clock_local">
+		<div class="hours-container">
+			<div class="hours"></div>
 		</div>
-	</div>
+		<div class="minutes-container">
+			<div class="minutes"></div>
+		</div>
+		<div class="seconds-container">
+			<div class="seconds"></div>
+		</div>
+	</article>
+<script src="/js/script_clock.js"></script>
 </div>
--- templates/time.tpl.php
+++ templates/time.tpl.php
@@ -4,6 +4,7 @@
 //if ($link_ad_position == 1 && ((isset($_COOKIE['NSH:member']) && $_COOKIE['NSH:member'] == get_member_email()) || $_SESSION['loggedin'] == 1)) {
 //	include template('link_ads_200x90');
 //}
+include template('clock');
 include template('sleep_time');
 //if ($link_ad_position == 2 && ((isset($_COOKIE['NSH:member']) && $_COOKIE['NSH:member'] == get_member_email()) || $_SESSION['loggedin'] == 1)) {
 //	include template('link_ads_200x90');
